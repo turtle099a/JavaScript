da79de5e11e5f59d2e55d2d07f1668f5f8cbdf13
turtle099a <151930653+turtle099a@users.noreply.github.com>
2023-11-24 15:24:07 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>Ваше портфоліо</title>
+</head>
+<body>
+    <!-- панель bootstrap -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Моє портфоліо</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Домашня</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Про мене</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Проекти</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Контакти</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <label for="customRange3" class="form-label">Example range</label>
+    <input type="range" class="form-range" min="0" max="5" step="0.5" id="customRange3">
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="card">
+                    <img src="img\ava.png" class="card-img-top mx-auto d-block img-fluid" style="width: 300px; height: 300px;"><div class="card-body">
+                        <h5 class="card-title">Strilak</h5>
+                        <p class="card-text">Тут типу інформація про мене</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<footer class="bg-dark text-white text-center py-3">
+        <p>футер</p>
+    </footer>
+</html>
